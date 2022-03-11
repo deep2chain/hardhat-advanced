081e895fee31c3a6fbe46489c9851079224b1846
gustav <colbertcrypto@outlook.com>
2022-03-11 22:40:00 +0800

    add pywallet submodule

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-dotenv.config();
+dotenv.config({path:__dirname+'/.env'});
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
